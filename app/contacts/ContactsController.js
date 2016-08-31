e6aa530f4af21f1a4baba274640320a4ee39aced
Antoniy Zyumbyulev <antonious7@gmail.com>
2016-08-31 11:57:31 +0100

    simplifying the UI

--- app/contacts/ContactsController.js
+++ app/contacts/ContactsController.js
@@ -14,7 +14,6 @@
         vm.title = "Simple contacts list";
         
         vm.contact = {};
-        vm.contactToEdit = undefined;
 
         vm.contacts = ContactsService.getAllContacts();
         
@@ -24,12 +23,11 @@
         }
 
         vm.setEditable = function(id, contact) {
-            vm.contactToEdit = angular.merge({},contact, {id : id});
+            vm.contact = angular.merge({},contact, {id : id});
         }
 
         vm.editContact = function(id, contact){
             ContactsService.editContact(contact.id, contact);
-            vm.contactToEdit = undefined;
         }
 
         vm.deleteContact = function(contact) {
